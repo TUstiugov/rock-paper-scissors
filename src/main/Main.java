dbcd57616f4553c0502922c35c79ed8de1d256c5
T. Ustiugov <t.ustiugov@gmail.com>
2024-02-24 11:52:20 +0200

    refactor: move enum Hand to separate file

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,31 +1,5 @@
 package main;
 
-import java.util.*;
-
-enum Hand {
-	ROCK, PAPER, SCISSORS, WELL;
-
-	private static Map<Hand, Set<Hand>> beatsMap = Map.of(
-		ROCK, Set.of(SCISSORS),
-		SCISSORS, Set.of(PAPER),
-		PAPER, Set.of(ROCK, WELL),
-		WELL, Set.of(ROCK, SCISSORS)
-	);
-
-	private static Random random = new Random();
-
-	boolean beats(Hand other) {
-		return beatsMap.get(this).contains(other);
-	}
-
-	static Hand random() {
-		Hand[] options = Hand.values();
-		int selected = random.nextInt(options.length);
-
-		return options[selected];
-	}
-}
-
 public class Main {
 	public static void main(String[] args) {
 		Hand first = Hand.random();
