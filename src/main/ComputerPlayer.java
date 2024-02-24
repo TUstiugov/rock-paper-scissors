70f123bacfd3b696b35940929097e0c68fc3e604
T. Ustiugov <t.ustiugov@gmail.com>
2024-02-24 12:03:46 +0200

    feat: add class ComputerPlayer

---
+++ src/main/ComputerPlayer.java
@@ -0,0 +1,12 @@
+package main;
+
+public class ComputerPlayer extends Player {
+	ComputerPlayer(String name) {
+		super(name);
+	}
+
+	@Override
+	protected Hand selectHand() {
+		return Hand.random();
+	}
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,8 +2,10 @@ package main;
 
 public class Main {
 	public static void main(String[] args) {
-		Hand first = Hand.random();
-		Hand second = Hand.random();
-		System.out.printf("%s %s %b", first, second, first.beats(second));
+		Player left = new ComputerPlayer("Left Player");
+		Player right = new ComputerPlayer("Right Player");
+		System.out.print(left.drawHand() + " ");
+		System.out.println(right.drawHand());
+		System.out.println(left.lastHand.beats(right.lastHand));
 	}
 }
